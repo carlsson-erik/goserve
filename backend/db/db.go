518e2d3670bcb812119e502ee970e334a9e3d78a
carlsson-erik <thezleepers@gmail.com>
2025-09-11 17:48:13 +0200

    fix migrations

--- backend/db/db.go
+++ backend/db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -14,14 +15,25 @@ func MigrateDB() {
 	db_url := os.Getenv("DB_URL")
 	db_port := os.Getenv("DB_PORT")
 	db_database := os.Getenv("DB_DATABASE")
+	db_user := os.Getenv("DB_USER")
+	db_password := os.Getenv("DB_PASSWORD")
 
 	time.Sleep(2 * time.Second)
 
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		db_user,
+		db_password,
+		db_url,
+		db_port,
+		db_database,
+	)
+
 	m, err := migrate.New(
 		"file://db/migrations",
-		"postgres://postgres:postgres@"+db_url+":"+db_port+"/"+db_database+"?sslmode=disable")
+		dsn)
 	if err != nil {
-		log.Printf("Failed migration to db: DB_URL: %v, DB_PORT: %v, DB_DATABASE: %v", db_url, db_port, db_database)
+		log.Printf("Failed migration to db: DB_URL: %s", dsn)
 		log.Fatal(err)
 	}
 
